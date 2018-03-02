649efe58949446b195e2bd2d5dd34a6d9888e46e
hirotakaster <hirotakaster@gmail.com>
2018-03-02 17:50:13 +0900

    add TLS private key(for Amazon AWS IoT...etc) sample file.

---
+++ examples/a2-example/a2-example.ino
@@ -0,0 +1,112 @@
+#include "MQTT-TLS.h"
+
+void callback(char* topic, byte* payload, unsigned int length);
+
+#define AMAZON_IOT_ROOT_CA_PEM                                          \
+"-----BEGIN CERTIFICATE----- \r\n"                                      \
+"MIIE0zCCA7ugAwIBAgIQGNrRniZ96LtKIVjNzGs7SjANBgkqhkiG9w0BAQUFADCB\r\n"  \
+"yjELMAkGA1UEBhMCVVMxFzAVBgNVBAoTDlZlcmlTaWduLCBJbmMuMR8wHQYDVQQL\r\n"  \
+"ExZWZXJpU2lnbiBUcnVzdCBOZXR3b3JrMTowOAYDVQQLEzEoYykgMjAwNiBWZXJp\r\n"  \
+"U2lnbiwgSW5jLiAtIEZvciBhdXRob3JpemVkIHVzZSBvbmx5MUUwQwYDVQQDEzxW\r\n"  \
+"ZXJpU2lnbiBDbGFzcyAzIFB1YmxpYyBQcmltYXJ5IENlcnRpZmljYXRpb24gQXV0\r\n"  \
+"aG9yaXR5IC0gRzUwHhcNMDYxMTA4MDAwMDAwWhcNMzYwNzE2MjM1OTU5WjCByjEL\r\n"  \
+"MAkGA1UEBhMCVVMxFzAVBgNVBAoTDlZlcmlTaWduLCBJbmMuMR8wHQYDVQQLExZW\r\n"  \
+"ZXJpU2lnbiBUcnVzdCBOZXR3b3JrMTowOAYDVQQLEzEoYykgMjAwNiBWZXJpU2ln\r\n"  \
+"biwgSW5jLiAtIEZvciBhdXRob3JpemVkIHVzZSBvbmx5MUUwQwYDVQQDEzxWZXJp\r\n"  \
+"U2lnbiBDbGFzcyAzIFB1YmxpYyBQcmltYXJ5IENlcnRpZmljYXRpb24gQXV0aG9y\r\n"  \
+"aXR5IC0gRzUwggEiMA0GCSqGSIb3DQEBAQUAA4IBDwAwggEKAoIBAQCvJAgIKXo1\r\n"  \
+"nmAMqudLO07cfLw8RRy7K+D+KQL5VwijZIUVJ/XxrcgxiV0i6CqqpkKzj/i5Vbex\r\n"  \
+"t0uz/o9+B1fs70PbZmIVYc9gDaTY3vjgw2IIPVQT60nKWVSFJuUrjxuf6/WhkcIz\r\n"  \
+"SdhDY2pSS9KP6HBRTdGJaXvHcPaz3BJ023tdS1bTlr8Vd6Gw9KIl8q8ckmcY5fQG\r\n"  \
+"BO+QueQA5N06tRn/Arr0PO7gi+s3i+z016zy9vA9r911kTMZHRxAy3QkGSGT2RT+\r\n"  \
+"rCpSx4/VBEnkjWNHiDxpg8v+R70rfk/Fla4OndTRQ8Bnc+MUCH7lP59zuDMKz10/\r\n"  \
+"NIeWiu5T6CUVAgMBAAGjgbIwga8wDwYDVR0TAQH/BAUwAwEB/zAOBgNVHQ8BAf8E\r\n"  \
+"BAMCAQYwbQYIKwYBBQUHAQwEYTBfoV2gWzBZMFcwVRYJaW1hZ2UvZ2lmMCEwHzAH\r\n"  \
+"BgUrDgMCGgQUj+XTGoasjY5rw8+AatRIGCx7GS4wJRYjaHR0cDovL2xvZ28udmVy\r\n"  \
+"aXNpZ24uY29tL3ZzbG9nby5naWYwHQYDVR0OBBYEFH/TZafC3ey78DAJ80M5+gKv\r\n"  \
+"MzEzMA0GCSqGSIb3DQEBBQUAA4IBAQCTJEowX2LP2BqYLz3q3JktvXf2pXkiOOzE\r\n"  \
+"p6B4Eq1iDkVwZMXnl2YtmAl+X6/WzChl8gGqCBpH3vn5fJJaCGkgDdk+bW48DW7Y\r\n"  \
+"5gaRQBi5+MHt39tBquCWIMnNZBU4gcmU7qKEKQsTb47bDN0lAtukixlE0kF6BWlK\r\n"  \
+"WE9gyn6CagsCqiUXObXbf+eEZSqVir2G3l6BFoMtEMze/aiCKm0oHw0LxOXnGiYZ\r\n"  \
+"4fQRbxC1lfznQgUy286dUV4otp6F01vvpX1FQHKOtw5rDgb7MzVIcbidJ4vEZV8N\r\n"  \
+"hnacRHr2lVz2XTIIM6RUthg/aFzyQkqFOFSDX9HoLPKsEdao7WNq\r\n"  \
+"-----END CERTIFICATE----- "
+const char amazonIoTRootCaPem[] = AMAZON_IOT_ROOT_CA_PEM;
+
+
+#define CELINT_KEY_CRT_PEM                                              \
+"-----BEGIN CERTIFICATE----- \r\n"                                      \
+"[YOUR CLIENT KEY CRT PEM]                                       \r\n"  \
+"-----END CERTIFICATE----- "
+const char clientKeyCrtPem[] = CELINT_KEY_CRT_PEM;
+
+
+#define CELINT_KEY_PEM                                                  \
+"-----BEGIN RSA PRIVATE KEY-----\r\n"                                   \
+"[YOUR RSA PRIVATE KEY PEM]                                      \r\n"  \
+"-----END RSA PRIVATE KEY----- "
+
+const char clientKeyPem[] = CELINT_KEY_PEM;
+
+
+/**
+ * if want to use IP address,
+ * byte server[] = { XXX,XXX,XXX,XXX };
+ * MQTT client(server, 1883, callback);
+ * want to use domain name,
+ * MQTT client("www.sample.com", 1883, callback);
+ **/
+MQTT client("[private key mqtt server]", 8883, callback);
+
+// recieve message
+void callback(char* topic, byte* payload, unsigned int length) {
+    char p[length + 1];
+    memcpy(p, payload, length);
+    p[length] = NULL;
+    String message(p);
+
+    if (message.equals("RED"))
+        RGB.color(255, 0, 0);
+    else if (message.equals("GREEN"))
+        RGB.color(0, 255, 0);
+    else if (message.equals("BLUE"))
+        RGB.color(0, 0, 255);
+    else
+        RGB.color(255, 255, 255);
+    delay(1000);
+}
+
+#define ONE_DAY_MILLIS (24 * 60 * 60 * 1000)
+unsigned long lastSync = millis();
+int counter = 0;
+void setup() {
+    if (millis() - lastSync > ONE_DAY_MILLIS) {
+        Particle.syncTime();
+        lastSync = millis();
+    }
+
+    RGB.control(true);
+
+    // enable tls. set Root CA pem, private key file.
+    client.enableTls(amazonIoTRootCaPem, sizeof(amazonIoTRootCaPem),
+                     clientKeyCrtPem, sizeof(clientKeyCrtPem),
+                     clientKeyPem, sizeof(clientKeyPem));
+    Serial.println("tls enable");
+
+    // connect to the server
+    client.connect("sparkclient");
+
+    // publish/subscribe
+    if (client.isConnected()) {
+        Serial.println("client connected");
+        client.publish("outTopic/message", "hello world");
+        client.subscribe("inTopic/message");
+    }
+}
+
+void loop() {
+    if (client.isConnected()) {
+        client.loop();
+    }
+    delay(200);
+}
